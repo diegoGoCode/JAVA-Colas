65679795398e94bd83bcce97a26339126828668a
Diego Diaz <diego.diaz@desarrollo.ddbcol.com.co>
2019-10-04 14:08:12 -0500

    Se creo, lainterfaz grafica y se agregaron eventos en los botones para que ejecutaran los metodos echos anteriormente

---
+++ src/colasestaticas/Main.java
@@ -0,0 +1,13 @@
+
+package colasestaticas;
+
+/**
+ *
+ * @author diego.diaz
+ */
+public class Main {
+    public static void main(String[] args) {
+        Inter formulario = new Inter();
+        formulario.setVisible(true);
+    }
+}
