bd9a6e6b133ab7427c94d5a504fdee0f2f28b800
Diego Diaz <diego.diaz@desarrollo.ddbcol.com.co>
2019-10-04 09:04:14 -0500

    Commit 1 - inicio del aplicativo

---
+++ src/colasestaticas/ColasEstaticas.java
@@ -0,0 +1,13 @@
+package colasestaticas;
+
+/**
+ *
+ * @author diego.diaz
+ */
+public class ColasEstaticas {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
